c632a91b0c69ad8ed61acff658b814d818d0133e
Valegl12 <vale12lunita@gmail.com>
2024-07-31 17:09:37 -0600

    Delete ejercicios directory

--- ejercicios/chicharronera.py
+++
@@ -1,7 +0,0 @@
-a = float (input ('Ingresa el valor de A'))
-b = float ( input ('Ingresa el valor de B'))
-c = float (input ('Ingresa el valor de C'))
-
-x1 = (-b +(b*b -4*a*c)**0.5)/(2*a)
-x2 = (-b -(b*b -4*a*c)**0.5)/(2*a)
-print(x1, '', x2)
